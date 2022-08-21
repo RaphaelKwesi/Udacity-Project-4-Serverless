fd1f64672597ad6aa7cc6dc2ce0ec90ead49c019
Raphael Agbozo <raphaelkwesi@gmail.com>
2022-08-21 17:42:42 +0000

    reinitialized files

---
+++ README.md
@@ -0,0 +1,2 @@
+<!-- My Service Endpoint. -->
+https://cyq28774ra.execute-api.us-east-2.amazonaws.com/prod
---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'cyq28774ra';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/prod`
+
+export const authConfig = {
+  domain: 'dev-ak959pp4.us.auth0.com',
+  clientId: 'wegemQaOaqaQ1q0hVDNpRvn6YzNaBjNY',
+  callbackUrl: 'http://localhost:3000/callback'
+};
